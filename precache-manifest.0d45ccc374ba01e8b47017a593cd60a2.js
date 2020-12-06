9719e966dd842f355160dab2c5fabc192dfd0643
Alex <alex@86degrees.com>
2020-12-06 21:26:23 +0200

    Updates

---
+++ precache-manifest.0d45ccc374ba01e8b47017a593cd60a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c28476cce4349ab45bd7e5654bb6f5f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bf291016612f76b99834",
+    "url": "/monster-rolodex/static/css/main.cbf382c9.chunk.css"
+  },
+  {
+    "revision": "28b6289de494d89e3501",
+    "url": "/monster-rolodex/static/js/2.656f7b53.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.656f7b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf291016612f76b99834",
+    "url": "/monster-rolodex/static/js/main.9efa1805.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
